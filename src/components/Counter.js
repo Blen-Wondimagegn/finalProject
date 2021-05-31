66981dfbc998e42195bb3cb35b8461af2d5c5431
blen_meku <mekuriablen@gmail.com>
2021-05-31 16:02:00 -0700

    finalreact

---
+++ src/components/Counter.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Counter = ({
+  count,
+  onDecrement,
+  onIncrement,
+  onInputNumberChange,
+  numberInputted,
+  onInputNumber,
+  timesClicked,
+}) => {
+  return (
+    <div>
+      <div>
+        <button onClick={onDecrement}>-</button>
+        <span>{count}</span>
+        <button onClick={onIncrement}>+</button>
+        <br />
+        <h3>
+          Times the increment/decrement buttons have been clicked:{" "}
+          {timesClicked}
+        </h3>
+        {/* <button onClick={onReset}>Reset</button> */}
+        <input
+          type="text"
+          onChange={(event) => onInputNumberChange(event.target.value)}
+          value={numberInputted}
+        />
+        <button onClick={onInputNumber}>Change Count Number</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
